89704d7b575da1f0bb5476a734cfa63ddb544e11
Jesse Nazario <jessenzr@gmail.com>
2017-09-02 11:54:07 -0400

    use private key from settings

--- index_board/captcha.py
+++ index_board/captcha.py
@@ -1,7 +1,8 @@
 import requests
+from lamentation_wall.settings import RECAPTCHA_PRIVATE_KEY
 
 
 def check_recaptcha(request):
-    payload = {'secret': 'YOU_SECRET_KEY', 'response': request.POST.get('g-recaptcha-response')}
+    payload = {'secret': RECAPTCHA_PRIVATE_KEY, 'response': request.POST.get('g-recaptcha-response')}
     return requests.post('https://www.google.com/recaptcha/api/siteverify', payload)\
                    .json().get('success', False)
